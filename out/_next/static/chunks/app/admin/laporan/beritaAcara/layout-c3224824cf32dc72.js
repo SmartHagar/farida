a915fb31d299e50844800ea7f2242819a1f340e7
SmartHagar <smarthagar@gmail.com>
2024-01-14 15:43:26 +0900

    mengganti base url

--- out/_next/static/chunks/app/admin/laporan/beritaAcara/layout-c3224824cf32dc72.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7428,9715,3140,7985,9168,9417,3945,1428,592,2222,3324,9416,3378,537,9385,8457,4371],{7155:function(){}},function(n){n.O(0,[9253,7698,1744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
--- out/_next/static/chunks/app/dosen/upload/absen/layout-495f0cfde128b37b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8457,9715,3140,7985,9168,9417,3945,1428,592,2222,3324,9416,7428,3378,537,9385,4371],{7155:function(){}},function(n){n.O(0,[9253,7698,1744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
