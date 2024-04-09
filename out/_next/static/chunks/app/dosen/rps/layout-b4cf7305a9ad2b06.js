4c8b6906072ec6efca7114dba7647dd55b5d721a
SmartHagar <smarthagar@gmail.com>
2024-04-09 17:05:35 +0900

    memperbaiki base url

--- out/_next/static/chunks/app/dosen/rps/layout-b4cf7305a9ad2b06.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3945,9715,3140,7985,9417,926,3415,3324,9416,3378,7428,537,9385,8457,4371],{7155:function(){}},function(n){n.O(0,[9253,7698,1744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
